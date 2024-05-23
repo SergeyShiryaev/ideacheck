daaa814b3451c6fbbe5cbde9d8b7dd1916b263e2
sergey.v.shiryaev <sergey.v.shiryaev@gmail.com>
2024-05-23 21:23:55 +0300

    Build

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { babel } from '@rollup/plugin-babel';
-
-export default defineConfig({
-  plugins: [babel({ extensions: ['.ts', '.tsx'], babelHelpers: 'bundled' }), react({ fastRefresh: false })],
-})
